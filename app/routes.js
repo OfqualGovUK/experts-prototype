3d150517830f0117c5de2b118dcb77a70145fd9a
Jesse Yuen <contactjesseyuen@gmail.com>
2022-09-23 11:01:48 +0100

    Content tweaks, width of review page fix and new screen added

--- app/routes.js
+++ app/routes.js
@@ -259,7 +259,7 @@ router.post('/add-another-answer', function (req, res) {
     if (addAnother === 'yes') {
       res.redirect('/register-your-interest/select-route')
     } else {
-      res.redirect('/register-your-interest/review') 
+      res.redirect('/register-your-interest/where-did-you-hear-about-experts') 
   }
 
 })
