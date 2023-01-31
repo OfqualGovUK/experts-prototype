1c5efea5f3422a67b9fbec493cd413a8524e7636
JackieDAlcorn <jacqueline.dalcorn@madetech.com>
2023-01-31 15:58:39 +0000

    start page changes

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply to be a subject matter expert',
+  serviceName: 'Subject matter experts',
 
   // Default port that prototype runs on
   port: '3000',
